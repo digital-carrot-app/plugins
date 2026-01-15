6a36985308ca71d536fe93847e4dd64149e1f5de
David N <gamma.dave@gmail.com>
2026-01-15 10:08:13 -0600

    fix oauth on fitbit

--- fitbit/plugin.js
+++ fitbit/plugin.js
@@ -7,18 +7,53 @@ function getLocalDateString() {
 }
 
 function activity_summary(config, params) {
-  http.setOauth(auth.auth(config.login));
-  const date = getLocalDateString();
-  var resp;
-  try {
-    resp = http.get(`/1/user/-/activities/date/${date}.json`);
-  } catch (e) {
-    console.log(e);
-    throw "Authentication failed. Please login again by going to Settings > Plugins > Fitbit";
+  var tokens = token_store.get();
+  if (tokens.original_refresh !== config.login.refresh_token) {
+    tokens = {
+      original_refresh: config.login.refresh_token,
+      access_token: config.login.access_token,
+      refresh_token: config.login.refresh_token,
+    };
+    token_store.save(tokens);
   }
+
+  authorized = http.withOptions({
+    headers: {
+      authorization: ["Bearer " + tokens.access_token],
+      "Content-Type": ["application/x-www-form-urlencoded"],
+    },
+  });
+
+  var isValid = authorized.post("/1.1/oauth2/introspect", {
+    string: `token=${tokens.access_token}`,
+  });
+
+  if (isValid.body.json.success === false) {
+    try {
+      authorized = http.withOptions({
+        headers: { "Content-Type": ["application/x-www-form-urlencoded"] },
+      });
+      var newCreds = authorized.post("/oauth2/token", {
+        string: `client_id=23TW6D&grant_type=refresh_token&refresh_token=${tokens.refresh_token}`,
+      });
+      console.log(newCreds.body.string);
+      tokens.access_token = newCreds.body.json.access_token;
+      tokens.refresh_token = newCreds.body.json.refresh_token;
+      token_store.save(tokens);
+
+      authorized = http.withOptions({
+        headers: { authorization: ["Bearer " + tokens.access_token] },
+      });
+    } catch (e) {
+      throw "Please login again by going to Settings > Plugins > Fitbit";
+    }
+  }
+
+  const date = getLocalDateString();
+  var resp = authorized.get(`/1/user/-/activities/date/${date}.json`);
+
   if (resp.statusCode != 200) {
-    console.log(resp.body.string);
-    throw "The fitbit API appears to be down. Please check back in later.";
+    throw "Failed to retrieve data from fitbit.";
   }
 
   const out = resp.body.json;
